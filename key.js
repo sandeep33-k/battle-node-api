cbe9d484121c3b151b55a40d030ad53333a97c14
sandeep_k <sandeep@interactionone.com>
2018-11-25 15:59:40 +0530

    updated credentails

--- key.js
+++ key.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-		_MONGODB_URL : process.env._MONGODB_URL || "mongodb://devBattle:mlabUser321@ds115154.mlab.com:15154/battle-node-dev",
+		_MONGODB_URL : process.env._MONGODB_URL || "mongodb://localhost/got_battle",
 		_API_USERNAME  : process.env._API_USERNAME || "battle_dev_user",
 		_API_PASSWORD  : process.env._API_PASSWORD || "BaTtL@User01",
 		_API_GRANT  : process.env._API_GRANT || "api"
